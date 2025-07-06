7c2e4634dc614be1945898734354166ac39de3ba
Krio Lyth <kriolyth@gmail.com>
2025-07-06 13:31:05 +0200

    Revert "Try a different way of autoversioning" and update the build actions instead
    This reverts commit f39e69a80396fc1b7abb0d9f8dbb42403f1f7962.

--- build.rs
+++ build.rs
@@ -2,15 +2,19 @@ use std::process::Command;
 
 fn export_git_commit_count() {
     let output = Command::new("git")
-        .args(&["log", "--pretty=oneline"])
+        .args(&["rev-list", "--count", "HEAD"])
         .output()
         .expect("Failed to execute git command");
 
     if output.status.success() {
-        let count = String::from_utf8_lossy(&output.stdout).chars().filter(|c| *c == '\n').count();
-        let mut version = format!("{:.5}", (count as f32).log(7.2));
-        let sub_ver = version.split_off(version.len() - 3);
-        println!("cargo:rustc-env=PACKAGE_VERSION={version}.{sub_ver}");
+        let maybe_count = String::from_utf8_lossy(&output.stdout).trim().parse::<u32>();
+        if let Ok(count) = maybe_count {      
+            let mut version = format!("{:.5}", (count as f32).log(7.2));
+            let sub_ver = version.split_off(version.len() - 3);
+            println!("cargo:rustc-env=PACKAGE_VERSION={version}.{sub_ver}");
+        } else {
+            println!("cargo:rustc-env=PACKAGE_VERSION=1.x");
+        }
     } else {
         println!("cargo:warning=Could not determine git commit count");
     }
