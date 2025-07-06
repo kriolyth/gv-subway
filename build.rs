870267c4704254f21f8ada94627924bc6227456c
Krio Lyth <kriolyth@gmail.com>
2025-07-06 14:43:34 +0200

    Autoversioning in actions

--- build.rs
+++ build.rs
@@ -1,22 +1,23 @@
 use std::process::Command;
 
 fn export_git_commit_count() {
-    let output = Command::new("git")
-        .args(&["rev-list", "--count", "HEAD"])
-        .output()
-        .expect("Failed to execute git command");
 
-    if output.status.success() {
-        let maybe_count = String::from_utf8_lossy(&output.stdout).trim().parse::<u32>();
-        if let Ok(count) = maybe_count {      
-            let mut version = format!("{:.5}", (count as f32).log(7.2));
-            let sub_ver = version.split_off(version.len() - 3);
-            println!("cargo:rustc-env=PACKAGE_VERSION={version}.{sub_ver}");
-        } else {
-            println!("cargo:rustc-env=PACKAGE_VERSION=1.x");
+    let num_commits: u32 = match std::env::var("GIT_NUM_COMMITS") {
+        Ok(result) => result.parse().unwrap_or(1),
+        Err(_) => {
+            let output = Command::new("git")
+                .args(&["rev-list", "--count", "HEAD"])
+                .output()
+                .expect("Failed to execute git command");
+            String::from_utf8_lossy(&output.stdout).trim().parse::<u32>().unwrap_or(1)
         }
+    };
+    if num_commits == 1 {
+        println!("cargo:rustc-env=PACKAGE_VERSION=1.x");
     } else {
-        println!("cargo:warning=Could not determine git commit count");
+        let mut version = format!("{:.5}", (num_commits as f32).log(7.2));
+        let sub_ver = version.split_off(version.len() - 3);
+        println!("cargo:rustc-env=PACKAGE_VERSION={version}.{sub_ver}");
     }
 }
 
