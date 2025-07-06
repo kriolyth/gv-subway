f39e69a80396fc1b7abb0d9f8dbb42403f1f7962
Krio Lyth <kriolyth@gmail.com>
2025-07-06 13:20:34 +0200

    Try a different way of autoversioning

--- build.rs
+++ build.rs
@@ -2,19 +2,15 @@ use std::process::Command;
 
 fn export_git_commit_count() {
     let output = Command::new("git")
-        .args(&["rev-list", "--count", "HEAD"])
+        .args(&["log", "--pretty=oneline"])
         .output()
         .expect("Failed to execute git command");
 
     if output.status.success() {
-        let maybe_count = String::from_utf8_lossy(&output.stdout).trim().parse::<u32>();
-        if let Ok(count) = maybe_count {      
-            let mut version = format!("{:.5}", (count as f32).log(7.2));
-            let sub_ver = version.split_off(version.len() - 3);
-            println!("cargo:rustc-env=PACKAGE_VERSION={version}.{sub_ver}");
-        } else {
-            println!("cargo:rustc-env=PACKAGE_VERSION=1.x");
-        }
+        let count = String::from_utf8_lossy(&output.stdout).chars().filter(|c| *c == '\n').count();
+        let mut version = format!("{:.5}", (count as f32).log(7.2));
+        let sub_ver = version.split_off(version.len() - 3);
+        println!("cargo:rustc-env=PACKAGE_VERSION={version}.{sub_ver}");
     } else {
         println!("cargo:warning=Could not determine git commit count");
     }
